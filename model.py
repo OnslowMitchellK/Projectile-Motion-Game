aac3d369917f3955938b4faff4cb240f28152a2c
OnslowOllieJoe <ollie.joe@student.onslow.school.nz>
2023-08-04 13:23:43 +1200

    Ollie 04/08

--- model.py
+++ model.py
@@ -1,35 +1,43 @@
 from dataclasses import dataclass
 import json
 import pygame
+from random import randint
 LEVEL_ONE_ENEMY_NUM = 2
+enemy_image = pygame.image.load("enemy.jpeg")
 
 
 @dataclass
 class Enemy():
     """Enemy Class."""
 
-    def __init__(self, name: str, health: int, damage: int):
+    def __init__(self, name: str, health: int,
+                 damage: int, x: int, y: int, width: int, height: int):
+        """Enemy variables"""
         self._name: str = name
         self._health: int = health
         self._damage: int = damage
+        self._x: int = x
+        self._y: int = y
+        self._width: int = width
+        self._height: int = height
 
     @property
     def name(self) -> str:
         """Name getter."""
         return self._name
-    
+
     @property
-    def health(self) -> str:
+    def health(self) -> int:
         """Health getter."""
         return self._health
 
     @property
-    def damage(self) -> str:
+    def damage(self) -> int:
         """Damage getter."""
         return self._damage
 
+
 level_one_enemies = {}
 for i in range(LEVEL_ONE_ENEMY_NUM):
-    level_one_enemies[i] = Enemy(f"Enemy {i}", 100, 25)
-enemy = Enemy("jamal", 100, 25)
-print(level_one_enemies[1].name)
+    level_one_enemies[i] = Enemy(f"Enemy {i}", 100, 25,
+                                 randint(0, 20), randint(0, 20), 40, 40)
--- test_screen.py
+++ test_screen.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from model import level_one_enemies, enemy_image
 # from os import join
 
 FPS = 60
@@ -12,72 +13,57 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.6)
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
-# background_image = pygame.image.load("background.jpg")
-
-# def get_background(background_name):
-#     image = pygame.image.load(join("assets", "Background", background_name))
-#     _, _, width, height = image.get_rect()
-#     tiles = []
-#     for i in range(SCREEN_WIDTH // width + 1):
-# 	    for j in range(SCREEN_HEIGHT // height + 1):
-# 		    pos = (i * width, j * height)
-#             tiles.append(pos)
-#     return tiles, image
+background_image = pygame.image.load("background.jpg")
 
 
-# def draw(window, background, bg_image):
-#     for tile in background:
-# 	    window.blit(bg_image, tile)
-	
-#     pygame.display.update()
+def main(window):
+    """Main."""
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            # Quit game.
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        # window.blit(background_image, [0, 0])
+        # pygame.display.flip()
+        window.blit(enemy_image, [SCREEN_WIDTH - level_one_enemies[0]._x, SCREEN_HEIGHT - enemy_image.getheight])
+        pygame.display.flip()
+
+        # draw(window, background, bg_image)
+    pygame.quit()
+    quit()
 
 
-# def main(screen):
-# 	clock = pygame.time.Clock()
-#     # background, bg_image = get_background("background.jpg")
-
-# 	run = True
-# 	while run:
-# 		clock.tick(FPS)
-# 		for event in pygame.event.get():
-# 			#quit game
-# 			if event.type == pygame.QUIT:
-# 				run = False
-# 				break
-# 		screen.blit(background_image, [0, 0])
-# 		pygame.display.flip()
-		
-#         # draw(window, background, bg_image)
-# 	pygame.quit()
-# 	quit()
+if __name__ == "__main__":
+    main(window)
 
 
-# if __name__ == "__main__":
-# 	main(window)
+# # Define floor parameters
+# FLOOR_WIDTH, FLOOR_HEIGHT = SCREEN_WIDTH, 100
+# FLOOR_COLOR = (0, 128, 0)  # Green color (RGB)
 
+# # Create the floor surface
+# floor_surface = pygame.Surface((FLOOR_WIDTH, FLOOR_HEIGHT))
+# floor_surface.fill(FLOOR_COLOR)
 
-# Define floor parameters
-FLOOR_WIDTH, FLOOR_HEIGHT = SCREEN_WIDTH, 100
-FLOOR_COLOR = (0, 128, 0)  # Green color (RGB)
+# # Main game loop
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
 
-# Create the floor surface
-floor_surface = pygame.Surface((FLOOR_WIDTH, FLOOR_HEIGHT))
-floor_surface.fill(FLOOR_COLOR)
+#     # Clear the screen
+#     window.fill((255, 255, 255))  # White background color
 
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+#     # Draw the floor at the bottom-center of the screen
+#     floor_x = (SCREEN_WIDTH - FLOOR_WIDTH) // 2
+#     floor_y = SCREEN_HEIGHT - FLOOR_HEIGHT
+#     window.blit(floor_surface, (floor_x, floor_y))
 
-    # Clear the screen
-    window.fill((255, 255, 255))  # White background color
-
-    # Draw the floor at the bottom-center of the screen
-    floor_x = (SCREEN_WIDTH - FLOOR_WIDTH) // 2
-    floor_y = SCREEN_HEIGHT - FLOOR_HEIGHT
-    window.blit(floor_surface, (floor_x, floor_y))
-
-    # Update the display
-    pygame.display.flip()
+#     # Update the display
+#     pygame.display.flip()
