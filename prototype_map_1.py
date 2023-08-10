e1683ea795304484032f8a00f6256b45307b5b95
damonsue <99217181+damonsue@users.noreply.github.com>
2023-08-11 08:22:28 +1200

    didn't fully save

--- prototype_map_1.py
+++ prototype_map_1.py
@@ -11,19 +11,19 @@ map_1_small = """
                                 
                                 
                                 
+                    2222        
                     1111        
-                    0000        
                                 
-                           11111
-                          100000
-                         1000000
-                        10000000
-                       100000000
-          111         1000000000
-         100011      10000000000
-11111111100000011111100000000000
-00000000000000000000000000000000
-00000000000000000000000000000000"""
+                           22222
+                          211111
+                         2111111
+                        21111111
+                       211111111
+          222         2111111111
+         211122      21111111111
+22222222211111122222211111111111
+11111111111111111111111111111111
+11111111111111111111111111111111"""
 
 map_1 = """                                                                
                                                                 
@@ -31,7 +31,6 @@ map_1 = """
                                                                 
                                                                 
                                                                 
-                                                22222222        
                                                                 
                                                                 
                                                                 
@@ -41,62 +40,64 @@ map_1 = """
                                                                 
                                                                 
                                                                 
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                                
-                                                            2222
-                                                           21111
-                                                         2111111
-                                                       211111111
-                                                     22111111111
-                                                  22211111111111
-                                                 211111111111111
-2222222222222222222222222222222222222222222222222111111111111111
+                         22222222222222222222222                
+                          111111111111111111111                 
+                            11111111111111111                   
+                              1111111111111                     
+                                111111111                       
+                                111111111                       
+                                111111111                       
+                               1111111111                       
+                               1111111111                       
+                               111111111                       
+                               111111111                        
+           22222222222         111111111                    2222
+           11111111111           11111111                  21111
+           11111111111          111111111                2211111
+           11111111111         1111111111              221111111
+           11111111111         11111111              22111111111
+           11111111111         111111111          22211111111111
+           11111111111         1111111111         21111111111111
+2222222222211111111111222222222211111111122222222111111111111111
 1111111111111111111111111111111111111111111111111111111111111111
 1111111111111111111111111111111111111111111111111111111111111111"""
 
 map_1_background = pygame.image.load("airport_background.png")
 map_1_background = pygame.transform.scale(map_1_background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
+TILE_SIZE = 20
+
 img_1 = pygame.image.load("tile1.png")
 img_2 = pygame.image.load("tile2.png")
-img1 = pygame.transform.scale(img_1, (20, 20))
-img2 = pygame.transform.scale(img_2, (20, 20))
+img_1 = pygame.transform.scale(img_1, (TILE_SIZE, TILE_SIZE))
+img_2 = pygame.transform.scale(img_2, (TILE_SIZE, TILE_SIZE))
 
 PROJECTILE_SIDE = 80
 projectile = pygame.image.load("test.png")
 projectile = pygame.transform.scale(projectile, (PROJECTILE_SIDE, PROJECTILE_SIDE))
 projectile_rect = None
-tile_rect = pygame.Rect(0, 0, 20, 20)
+tile_rect = pygame.Rect(0, 0, TILE_SIZE, TILE_SIZE)
 tile_rects = []
 
-def make_window(width: int, height:int, caption: str) -> pygame.Surface:
+def make_window(width: int, height:int, caption: str)  -> pygame.Surface:
     win = pygame.display.set_mode((width, height))
     pygame.display.set_caption(caption)
     return win
 
 def draw_tiles(map):
     game_map = map.split("\n")
-    print(game_map[6])
     x = 0
     y = 0
     for row in game_map:
         x = 0
         for tile in row:
-            tile_rect.x = x * 20
-            tile_rect.y = y * 20
+            tile_rect.x = x * TILE_SIZE
+            tile_rect.y = y * TILE_SIZE
             if tile == "1":
-                window.blit(img_1, (x * 20, y * 20))
+                window.blit(img_1, (x * TILE_SIZE, y * TILE_SIZE))
                 tile_rects.append(tile_rect.copy())
             elif tile == "2":
-                window.blit(img_2, (x * 20, y * 20))
+                window.blit(img_2, (x * TILE_SIZE, y * TILE_SIZE))
                 tile_rects.append(tile_rect.copy())
             x += 1
         y += 1
@@ -118,7 +119,6 @@ def main():
     x = start_x
     y = start_y
     projectile_rect = pygame.Rect(start_x + 0.25 * PROJECTILE_SIDE, start_y + 0.25 * PROJECTILE_SIDE, 40, 40)
-    pygame.draw.rect(window, "pink", projectile_rect)
     window.blit(projectile, (x, y))
     draw_tiles(map_1)
     pygame.display.update()
